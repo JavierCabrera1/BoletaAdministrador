f7bbc2d8c3a143b480fe933eee538b4ebec730db
Javier Cabrera <jcabrera-2019045@kinal.edu.gt>
2021-09-14 11:56:29 -0600

    Commit Inicial

---
+++ BoletasGerencia/BoletasUsuario/DetallesMarca.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace BoletasUsuario
+{
+    partial class DetallesMarca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titulo_lbl = new System.Windows.Forms.Label();
+            this.nombre_lbl = new System.Windows.Forms.Label();
+            this.nombre_var_lbl = new System.Windows.Forms.Label();
+            this.hora_var_lbl = new System.Windows.Forms.Label();
+            this.hora_lbl = new System.Windows.Forms.Label();
+            this.fecha_var_lbl = new System.Windows.Forms.Label();
+            this.fecha_lbl = new System.Windows.Forms.Label();
+            this.tipo_var_lbl = new System.Windows.Forms.Label();
+            this.tipo_lbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.depto_lbl = new System.Windows.Forms.Label();
+            this.depto_var_lbl = new System.Windows.Forms.Label();
+            this.foto_dgv = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.foto_dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titulo_lbl
+            // 
+            this.titulo_lbl.AutoSize = true;
+            this.titulo_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.titulo_lbl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.titulo_lbl.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo_lbl.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.titulo_lbl.Location = new System.Drawing.Point(364, 74);
+            this.titulo_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titulo_lbl.Name = "titulo_lbl";
+            this.titulo_lbl.Size = new System.Drawing.Size(321, 40);
+            this.titulo_lbl.TabIndex = 0;
+            this.titulo_lbl.Text = "Detalle Marca Usuario";
+            // 
+            // nombre_lbl
+            // 
+            this.nombre_lbl.AutoSize = true;
+            this.nombre_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.nombre_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombre_lbl.Location = new System.Drawing.Point(16, 25);
+            this.nombre_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nombre_lbl.Name = "nombre_lbl";
+            this.nombre_lbl.Size = new System.Drawing.Size(75, 21);
+            this.nombre_lbl.TabIndex = 1;
+            this.nombre_lbl.Text = "Nombre:";
+            // 
+            // nombre_var_lbl
+            // 
+            this.nombre_var_lbl.AutoSize = true;
+            this.nombre_var_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.nombre_var_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombre_var_lbl.Location = new System.Drawing.Point(122, 25);
+            this.nombre_var_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nombre_var_lbl.Name = "nombre_var_lbl";
+            this.nombre_var_lbl.Size = new System.Drawing.Size(71, 21);
+            this.nombre_var_lbl.TabIndex = 2;
+            this.nombre_var_lbl.Text = "Nombre";
+            // 
+            // hora_var_lbl
+            // 
+            this.hora_var_lbl.AutoSize = true;
+            this.hora_var_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.hora_var_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hora_var_lbl.Location = new System.Drawing.Point(86, 74);
+            this.hora_var_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.hora_var_lbl.Name = "hora_var_lbl";
+            this.hora_var_lbl.Size = new System.Drawing.Size(46, 21);
+            this.hora_var_lbl.TabIndex = 4;
+            this.hora_var_lbl.Text = "Hora";
+            // 
+            // hora_lbl
+            // 
+            this.hora_lbl.AutoSize = true;
+            this.hora_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.hora_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hora_lbl.Location = new System.Drawing.Point(16, 74);
+            this.hora_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.hora_lbl.Name = "hora_lbl";
+            this.hora_lbl.Size = new System.Drawing.Size(50, 21);
+            this.hora_lbl.TabIndex = 3;
+            this.hora_lbl.Text = "Hora:";
+            // 
+            // fecha_var_lbl
+            // 
+            this.fecha_var_lbl.AutoSize = true;
+            this.fecha_var_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.fecha_var_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fecha_var_lbl.Location = new System.Drawing.Point(453, 74);
+            this.fecha_var_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.fecha_var_lbl.Name = "fecha_var_lbl";
+            this.fecha_var_lbl.Size = new System.Drawing.Size(52, 21);
+            this.fecha_var_lbl.TabIndex = 6;
+            this.fecha_var_lbl.Text = "Fecha";
+            // 
+            // fecha_lbl
+            // 
+            this.fecha_lbl.AutoSize = true;
+            this.fecha_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.fecha_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fecha_lbl.Location = new System.Drawing.Point(360, 74);
+            this.fecha_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.fecha_lbl.Name = "fecha_lbl";
+            this.fecha_lbl.Size = new System.Drawing.Size(56, 21);
+            this.fecha_lbl.TabIndex = 5;
+            this.fecha_lbl.Text = "Fecha:";
+            // 
+            // tipo_var_lbl
+            // 
+            this.tipo_var_lbl.AutoSize = true;
+            this.tipo_var_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.tipo_var_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipo_var_lbl.Location = new System.Drawing.Point(861, 74);
+            this.tipo_var_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.tipo_var_lbl.Name = "tipo_var_lbl";
+            this.tipo_var_lbl.Size = new System.Drawing.Size(43, 21);
+            this.tipo_var_lbl.TabIndex = 8;
+            this.tipo_var_lbl.Text = "Tipo";
+            // 
+            // tipo_lbl
+            // 
+            this.tipo_lbl.AutoSize = true;
+            this.tipo_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.tipo_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipo_lbl.Location = new System.Drawing.Point(684, 74);
+            this.tipo_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.tipo_lbl.Name = "tipo_lbl";
+            this.tipo_lbl.Size = new System.Drawing.Size(47, 21);
+            this.tipo_lbl.TabIndex = 7;
+            this.tipo_lbl.Text = "Tipo:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.depto_lbl);
+            this.groupBox1.Controls.Add(this.depto_var_lbl);
+            this.groupBox1.Controls.Add(this.nombre_var_lbl);
+            this.groupBox1.Controls.Add(this.tipo_var_lbl);
+            this.groupBox1.Controls.Add(this.nombre_lbl);
+            this.groupBox1.Controls.Add(this.tipo_lbl);
+            this.groupBox1.Controls.Add(this.hora_lbl);
+            this.groupBox1.Controls.Add(this.fecha_var_lbl);
+            this.groupBox1.Controls.Add(this.hora_var_lbl);
+            this.groupBox1.Controls.Add(this.fecha_lbl);
+            this.groupBox1.Location = new System.Drawing.Point(375, 140);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(1047, 140);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // depto_lbl
+            // 
+            this.depto_lbl.AutoSize = true;
+            this.depto_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.depto_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.depto_lbl.Location = new System.Drawing.Point(684, 25);
+            this.depto_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.depto_lbl.Name = "depto_lbl";
+            this.depto_lbl.Size = new System.Drawing.Size(120, 21);
+            this.depto_lbl.TabIndex = 9;
+            this.depto_lbl.Text = "Departamento:";
+            // 
+            // depto_var_lbl
+            // 
+            this.depto_var_lbl.AutoSize = true;
+            this.depto_var_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.depto_var_lbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.depto_var_lbl.Location = new System.Drawing.Point(861, 25);
+            this.depto_var_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.depto_var_lbl.Name = "depto_var_lbl";
+            this.depto_var_lbl.Size = new System.Drawing.Size(116, 21);
+            this.depto_var_lbl.TabIndex = 10;
+            this.depto_var_lbl.Text = "Departamento";
+            // 
+            // foto_dgv
+            // 
+            this.foto_dgv.AllowUserToAddRows = false;
+            this.foto_dgv.AllowUserToDeleteRows = false;
+            this.foto_dgv.AllowUserToResizeColumns = false;
+            this.foto_dgv.AllowUserToResizeRows = false;
+            this.foto_dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.foto_dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.foto_dgv.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.foto_dgv.BackgroundColor = System.Drawing.Color.White;
+            this.foto_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.foto_dgv.ColumnHeadersVisible = false;
+            this.foto_dgv.Enabled = false;
+            this.foto_dgv.Location = new System.Drawing.Point(375, 289);
+            this.foto_dgv.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.foto_dgv.MultiSelect = false;
+            this.foto_dgv.Name = "foto_dgv";
+            this.foto_dgv.ReadOnly = true;
+            this.foto_dgv.RowHeadersVisible = false;
+            this.foto_dgv.Size = new System.Drawing.Size(1053, 628);
+            this.foto_dgv.TabIndex = 10;
+            // 
+            // DetallesMarca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BoletasUsuario.Properties.Resources.FONDO_4;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1794, 1020);
+            this.Controls.Add(this.foto_dgv);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.titulo_lbl);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "DetallesMarca";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DetallesMarca";
+            this.Load += new System.EventHandler(this.DetallesMarca_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.foto_dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titulo_lbl;
+        private System.Windows.Forms.Label nombre_lbl;
+        private System.Windows.Forms.Label nombre_var_lbl;
+        private System.Windows.Forms.Label hora_var_lbl;
+        private System.Windows.Forms.Label hora_lbl;
+        private System.Windows.Forms.Label fecha_var_lbl;
+        private System.Windows.Forms.Label fecha_lbl;
+        private System.Windows.Forms.Label tipo_var_lbl;
+        private System.Windows.Forms.Label tipo_lbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label depto_lbl;
+        private System.Windows.Forms.Label depto_var_lbl;
+        private System.Windows.Forms.DataGridView foto_dgv;
+    }
+}
---
+++ BoletasGerencia/BoletasUsuario/Marcas.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace BoletasUsuario
+{
+    public partial class Marcas : Form
+    {
+        #region Constructor
+        public Marcas()
+        {
+            InitializeComponent();
+        }
+        #endregion
+        #region Variables
+        ConsultasSQL sql = new ConsultasSQL();
+        #endregion
+        #region Load
+        private void Marcas_Load(object sender, EventArgs e)
+        {
+            marcas_dgv.DataSource = sql.ObtenerMarcas();
+            DataGridViewColumn colIdUsuario = marcas_dgv.Columns[6];
+            DataGridViewColumn colHora = marcas_dgv.Columns[7];
+            colIdUsuario.Visible = false;
+            colHora.Visible = false;
+            usuario_cmb.DataSource = sql.MostrarUsuarios();
+            usuario_cmb.DisplayMember = "nombre";
+            usuario_cmb.ValueMember = "idUsuario";
+            usuario_cmb.Enabled = false;
+        }
+        #endregion
+        #region Click Boton Buscar
+        private void buscar_btn_Click(object sender, EventArgs e)
+        {
+            if(buscar_id_chb.Checked == true)
+            {
+                string id = usuario_cmb.SelectedValue.ToString();
+                marcas_dgv.DataSource = sql.ObtenerMarcasPorID(id);
+            }
+            else
+            {
+                string fInicial = desde_dtp.Value.ToString("yyyy-MM-dd");
+                string fFinal = hasta_dtp.Value.ToString("yyyy-MM-dd");
+                marcas_dgv.DataSource = sql.ObtenerMarcasPorFecha(fInicial, fFinal);
+            }
+           
+        }
+        #endregion
+        #region Click Checkbox Buscar Por Usuario
+        private void buscar_id_chb_CheckedChanged(object sender, EventArgs e)
+        {
+            if (buscar_id_chb.Checked == true)
+            {
+                usuario_cmb.Enabled = true;
+                hasta_dtp.Enabled = false;
+                desde_dtp.Enabled = false;
+            }
+            else
+            {
+                usuario_cmb.Enabled = false;
+                hasta_dtp.Enabled = true;
+                desde_dtp.Enabled = true;
+            }
+        }
+        #endregion
+        #region Click Boton Excel
+        private void excel_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (marcas_dgv.DataSource != null)
+                {
+                    marcas_dgv.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+                    marcas_dgv.MultiSelect = true;
+                    marcas_dgv.SelectAll();
+                    DataObject dataObj = marcas_dgv.GetClipboardContent();
+                    if (dataObj != null)
+                        Clipboard.SetDataObject(dataObj);
+
+                    Microsoft.Office.Interop.Excel.Application xlexcel;
+                    Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+                    Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+                    object misValue = System.Reflection.Missing.Value;
+                    xlexcel = new Excel.Application();
+                    xlWorkBook = xlexcel.Workbooks.Add(misValue);
+                    xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+                    Excel.Range rango = (Excel.Range)xlWorkSheet.Cells[1, 1];
+                    rango.Select();
+                    xlWorkSheet.PasteSpecial(rango, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+
+                    //  la primera fila en negrita, centrada y con fondo gris
+                    Excel.Range fila1 = (Excel.Range)xlWorkSheet.Rows[1];
+                    fila1.Select();
+                    fila1.EntireRow.Font.Bold = true;
+                    fila1.EntireRow.HorizontalAlignment = HorizontalAlignment.Center;
+                    // si la primera celda de la primera columna está vacía, elimino la primera columna
+                    Excel.Range c1f1 = (Excel.Range)xlWorkSheet.Cells[1, 1];
+                    if (c1f1.Text == "")
+                    {
+                        Excel.Range columna1 = (Excel.Range)xlWorkSheet.Columns[1];
+                        columna1.Select();
+                        columna1.Delete();
+                    }
+
+                    //selecciono la primera celda de la primera columna
+                    Excel.Range c1 = (Excel.Range)xlWorkSheet.Cells[1, 1];
+                    c1.Select();
+                    xlexcel.Cells.Select();
+                    xlexcel.Cells.EntireColumn.AutoFit();
+                    xlexcel.Visible = true;
+                    marcas_dgv.ClearSelection();
+                    marcas_dgv.MultiSelect = false;
+                }
+                else
+                {
+                    MessageBox.Show("No hay nada en la tabla...", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+        #region Doble Click Celda
+        private void marcas_dgv_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex == -1) return;
+            DataGridViewRow fila = marcas_dgv.Rows[e.RowIndex];
+            string nombre_usuario = Convert.ToString(fila.Cells[1].Value);
+            string depto = Convert.ToString(fila.Cells[2].Value);
+            string fecha = Convert.ToString(fila.Cells[3].Value);
+            string hora = Convert.ToString(fila.Cells[4].Value);
+            string tipo = Convert.ToString(fila.Cells[5].Value);
+            string idUsuario = Convert.ToString(fila.Cells[6].Value);
+            DateTime horaSinFormato = Convert.ToDateTime(fila.Cells[7].Value);
+            DetallesMarca detallesMarca = new DetallesMarca();
+            detallesMarca.nombre_usuario = nombre_usuario;
+            detallesMarca.depto = depto;
+            detallesMarca.fecha = fecha;
+            detallesMarca.hora = hora;
+            detallesMarca.tipo = tipo;
+            detallesMarca.idUsuario = idUsuario;
+            detallesMarca.horaSinFormato = horaSinFormato;
+            detallesMarca.Show();        
+        }
+        #endregion
+    }
+}
